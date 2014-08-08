69b311fad0f1474dedc970ec8e4df6f4827e22ad
Martin Sutherland <martin@sunpig.com>
2014-08-08 20:39:47 +0200

    Remove navigation menu

--- functions.php
+++ functions.php
@@ -51,13 +51,6 @@ function ari_setup() {
 	add_theme_support( 'automatic-feed-links' );
 
 	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'ari' ),
-	) );
-
-	/**
 	 * This theme allows users to set a custom background.
 	 */
 	add_theme_support( 'custom-background' );
@@ -99,8 +92,6 @@ function ari_scripts() {
 	global $post;
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -162,8 +153,7 @@ function ari_custom_background() {
 	endif;
 ?>
 	<style type="text/css">
-		#page,
-		.main-navigation ul ul {
+		#page {
 			background-color: #<?php echo $background_color; ?>
 		}
 	</style>
--- sidebar.php
+++ sidebar.php
@@ -7,15 +7,6 @@
  */
 ?>
 
-<?php if ( has_nav_menu( 'primary' ) ) : ?>
-	<nav role="navigation" class="site-navigation main-navigation">
-		<h1 class="assistive-text"><?php _e( 'Menu', 'ari' ); ?></h1>
-		<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'ari' ); ?>"><?php _e( 'Skip to content', 'ari' ); ?></a></div>
-
-		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-	</nav>
-<?php endif; ?>
-
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<div id="secondary" class="widget-area" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
